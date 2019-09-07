b536e63ea21ab979f54a2f23cea1b31b460b3b58
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-08 00:19:53 +0600

    add comments to post

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,49 @@
 class ArticlesController < ApplicationController
+	def index
+		@articles = Article.all
+	end
+
 	def new
 	end
-	def create
-		render plain: params[:article].inspect
+	
+	def update
+		@article = Article.find(params[:id])
+	
+		if @article.update(article_params)
+			redirect_to @article
+		else
+			render action: 'edit'
+		end
 	end
+
+	def edit
+		@article = Article.find(params[:id])
+	end
+	
+	def show
+    @article = Article.find(params[:id])
+	end
+	
+  def create
+    @article = Article.new(article_params)
+    if @article.valid?
+      @article.save
+      redirect_to @article
+    else
+      render action: 'new'
+    end
+	end
+	
+	def destroy
+		@article = Article.find(params[:id])
+		@article.destroy
+	
+		redirect_to articles_path
+	end
+
+  private
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   get 'home/index'
-  get 'contacts' => 'contacts#new'
+	get 'contacts' => 'contacts#new'
+	get 'terms' => 'pages#terms'
+  get 'about' => 'pages#about'
   resource :contacts, only: [:new, :create],path_names: {:new => ''}
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-	resources :articles
+	resources :articles do 
+		resources :comments
+	end
 end
