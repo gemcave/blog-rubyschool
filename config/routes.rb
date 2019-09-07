f14d1dd748faa04c4fad0ff6b3a349b34e83aec7
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-07 14:51:52 +0600

    add contacts controller and :new view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'home/index'
+  resource :contacts, only: [:new, :create]
 	# For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 	resources :articles
 end
