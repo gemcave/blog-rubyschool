8f0910e3ed0a032ed51414f12885ae492d511854
Stepan Smykovskiy <stevendrosse@gmail.com>
2019-09-07 15:40:59 +0600

    save contact on submit

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,5 +2,10 @@ class ContactsController < ApplicationController
 	def new
 	end
 	def create
+		@contact = Contact.new(contact_params)
 	end
+
+	private
+	def contact_params
+		params.require(:contact).permit(:email, :message)
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_06_172215) do
+ActiveRecord::Schema.define(version: 2019_09_07_085341) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "text"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string "email"
+    t.text "message"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
